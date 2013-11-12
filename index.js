58e214ecc7e0bfe4667408c258f1a4113d88720c
Thorsten Lorenz <thlorenz@gmx.de>
2013-11-12 16:39:03 -0500

    resolving port and launching phantom only after zulu is up

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 var asyncreduce = require('asyncreduce')
   , resolveBin = require('resolve-bin')
   , spawn = require('child_process').spawn
+  , format = require('util').format
+
+function getZuluPort(args) {
+  var local = args.indexOf('--local');
+  if (!~local) return undefined;
+  return args[local + 1];
+}
 
 function resolveBins(names, cb) {
   asyncreduce(
@@ -19,21 +26,18 @@ function resolveBins(names, cb) {
   )
 }
 
-function inspect(obj, depth) {
-  console.error(require('util').inspect(obj, false, depth || 5, true));
-}
-
-function run(bin, args, cb) {
+function run(bin, args, env, cb) {
   var prog = spawn(
       bin
     , args
-    , { stdio: 'inherit' }
+    , { env: env }
   )
+  prog.stdout.pipe(process.stdout);
+  prog.stderr.pipe(process.stderr);
 
-  inspect(prog);
   prog
     .on('close', function (code) {
-      if (code !== 0) return cb(new Error('prog ' + args.join(' ') + ' returned with code ' + code));
+      if (code !== 0) return cb(new Error('prog ' + args.join(' ') + ' returned with code ' + code), code);
       cb();
     })
 
@@ -42,22 +46,34 @@ function run(bin, args, cb) {
 
 var go = module.exports = function (argv, env, cb) {
   var args = argv.slice(2);
+  var port = getZuluPort(args);
+  if (!port) return cb(new Error('Please provide a port for the zulu server, i.e. --local 3000'), -1);
+
   resolveBins([ 'zuul', 'mocha-phantomjs'], function (err, bins) {
+    var zuul;
+
     if (err) return cb(err);
-    var zuul = run(bins.zuul, args, function (err) {
-      if (err) return cb(err);
-      console.log('zuul finished');
+
+    zuul = run(bins.zuul, args, env, function (err, code) {
+      if (err) return cb(err, code);
     });
 
-    console.log('started zuul');
-    process.kill(zuul);
+    zuul.stdout.once('data', function (d) {
+      var route = format('http://localhost:%d/__zuul', port);
+
+      var mp = run(bins['mocha-phantomjs'], [ route ], env, function (err, code) {
+        process.kill(zuul);
+        cb(err, code);
+      });
+    });
   });    
 };
 
 
 // Test
 if (!module.parent && typeof window === 'undefined') {
-  go(process.argv, process.env, function (err) {
-    if (err) return console.error(err);
+  go(process.argv, process.env, function (err, code) {
+    if (err) console.error(err);
+    process.exit(code);
   });
 }
