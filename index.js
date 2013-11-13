e481039e041bfec55988b1c033b9316d5769ed8d
Thorsten Lorenz <thlorenz@gmx.de>
2013-11-13 10:05:40 -0500

    waiting for zuul before starting phantomjs

--- index.js
+++ index.js
@@ -30,14 +30,16 @@ function run(bin, args, cwd, cb) {
   var prog = spawn(
       bin
     , args
-    , { stdio: 'inherit', cwd: cwd }
+    , { cwd: cwd }
   )
 
-  prog
-    .on('close', function (code) {
-      if (code !== 0) return cb(new Error('prog ' + args.join(' ') + ' returned with code ' + code), code);
-      cb(null, code);
-    })
+  prog.stdout.pipe(process.stdout);
+  prog.stderr.pipe(process.stderr);
+
+  prog.on('close', function (code) {
+    if (code !== 0) return cb(new Error('prog ' + args.join(' ') + ' returned with code ' + code), code);
+    cb(null, code);
+  })
 
   return prog;
 }
@@ -58,20 +60,18 @@ var go = module.exports = function (args, cwd, cb) {
   var route = format('http://localhost:%d/__zuul', port);
 
   resolveBins([ 'zuul', 'mocha-phantomjs'], function (err, bins) {
-    var zuul, mp;
-
     if (err) return cb(err);
 
-    zuul = run(bins.zuul, args, cwd, function (err, code) {
-      if (err) {
-        if (mp) mp.kill();
-        return cb(err, code);
-      }
+    var zuul = run(bins.zuul, args, cwd, function (err, code) {
+      if (err) return cb(err, code);
     });
 
-    mp = run(bins['mocha-phantomjs'], [route].concat(args), cwd, function (err, code) {
-      zuul.kill();
-      cb(err, code);
+    // wait for zuul to start up and tell us to point our browser at the url
+    zuul.stdout.once('data', function () {
+      run(bins['mocha-phantomjs'], [route].concat(args), cwd, function (err, code) {
+        zuul.kill();
+        cb(err, code);
+      });
     });
   });    
 };
